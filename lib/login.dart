1472c79324c1e7089b56bb510fc9a17373759913
Shabalin13 <d.shabalin@innopolis.university>
2022-03-24 00:17:54 +0300

    add login page and modified register page

--- lib/login.dart
+++ lib/login.dart
@@ -1,18 +1,162 @@
-
-
+import 'package:arithmetic_pvp/logic/network_client.dart';
+import 'package:arithmetic_pvp/main.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class LoginPage extends StatefulWidget{
+import 'logic/auth.dart';
 
+class LoginPage extends StatefulWidget {
   @override
-  State<LoginPage> createState() =>  _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage>{
+class _LoginPageState extends State<LoginPage> {
+  final _registerFormKey = GlobalKey<FormState>();
+
+  bool _passwordObscure = true;
+
+  bool _submitBtnDisabled = false;
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  late Auth auth_client;
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  void initState() {
+    super.initState();
+    auth_client = Auth(NetworkClient());
   }
+
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+      body: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  "assets/dark_logo.svg",
+                  height: 128,
+                  width: 128,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Arithmetic PvP",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Text(
+                  "Sign In",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  child: Form(
+                    key: _registerFormKey,
+                    child: Column(
+                      children: [
+                        // Username/Nickname
+                        TextFormField(
+                          controller: _usernameController,
+                          validator: (value) {
+                            // some  nickname validation
+                            if (value == null || value.isEmpty) {
+                              return "Enter your username";
+                            }
+                            return null;
+                          },
+                          decoration: const InputDecoration(
+                              labelText: "Username",
+                              border: OutlineInputBorder(),
+                              hintText: "Nagibator228",
+                              fillColor: Color(0xFFE2E1E1),
+                              filled: true),
+                        ),
+                        // Mail
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          obscureText: _passwordObscure,
+                          decoration: InputDecoration(
+                              labelText: "Password",
+                              border: const OutlineInputBorder(),
+                              fillColor: Color(0xFFE2E1E1),
+                              filled: true,
+                              suffixIcon: IconButton(
+                                icon: Icon(_passwordObscure
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                onPressed: () {
+                                  setState(() {
+                                    _passwordObscure = !_passwordObscure;
+                                  });
+                                },
+                              )),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Enter your password";
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                            onPressed: () async {
+                              _submitBtnDisabled = true;
+                              if (_registerFormKey.currentState!.validate()) {
+                                var responseMap = await auth_client.login(
+                                    _usernameController.text,
+                                    _passwordController.text);
+                                var report = responseMap.keys.first;
+                                var status = responseMap[report] ?? false;
+                                // if (status) {
+                                if (true) {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => MyHomePage()));
+                                } else {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: Text(report)),
+                                  );
+                                }
+                              }
+                              _submitBtnDisabled = false;
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.black,
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 35, vertical: 15),
+                            ),
+                            child: const Text(
+                              "Login",
+                              style: TextStyle(fontSize: 18),
+                            )),
+                      ],
+                    ),
+                  ),
+                  width: 300,
+                )
+              ],
+            ),
+            ))));
+}
 }
