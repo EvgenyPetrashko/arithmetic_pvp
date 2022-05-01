62e727f2170a53c39575b0d25305c314a43cad77
EvgenyPetrashko <e.petrashko@innopolis.university>
2022-05-01 15:35:30 +0300

    Refactoring + removing parts where we set cookie to client headers (we don't need to do it multiple times, since we already done with it in main bloc)

--- lib/data/network_client.dart
+++ lib/data/network_client.dart
@@ -1,11 +1,11 @@
 import 'package:dio/dio.dart';
 
 class NetworkClient{
-  final baseUrl = "https://arithmetic-pvp-backend.herokuapp.com/";
-  final baseUrlTest = "http://192.168.31.116:8000/";
+  final _baseUrl = "https://arithmetic-pvp-backend.herokuapp.com/";
+  final _baseUrlTest = "http://192.168.31.116:8000/";
   late Dio api;
 
   NetworkClient(){
-    api = Dio(BaseOptions(baseUrl: baseUrl));
+    api = Dio(BaseOptions(baseUrl: _baseUrl));
   }
 }
