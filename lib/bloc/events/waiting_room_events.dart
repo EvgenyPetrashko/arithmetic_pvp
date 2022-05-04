6c1d38b2b14dfe5a294f3d3a6606d3e66f0d0184
Shabalin13 <d.shabalin@innopolis.university>
2022-05-04 11:48:36 +0300

    added bloc for waiting room

---
+++ lib/bloc/events/waiting_room_events.dart
@@ -0,0 +1,3 @@
+abstract class WaitingRoomEvent {}
+
+class EventUsersLoading extends WaitingRoomEvent {}
---
+++ lib/bloc/waiting_room_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:arithmetic_pvp/bloc/states/waiting_room_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import '../data/api.dart';
+import '../data/models/user.dart';
+import 'events/waiting_room_events.dart';
+
+class WaitingRoomBloc extends Bloc<WaitingRoomEvent, WaitingRoomState> {
+
+  WaitingRoomBloc() : super(UsersStateLoading()) {
+    final Api _api = GetIt.instance<Api>();
+
+    // on<EventUsersLoading>(
+    //   (event, emit) async {
+    //     final List<Profile> users = await _api.getUsers();
+    //     if (users.isEmpty) {
+    //       emit(UsersStateError("No users to display"));
+    //     } else {
+    //       emit(UsersStateLoaded(users));
+    //     }
+    //   },
+    // );
+
+  }
+}
