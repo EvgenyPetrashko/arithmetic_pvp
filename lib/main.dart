baed30768709d8cbe964875fc2be5d2ab4b5f3c2
EvgenyPetrashko <e.petrashko@innopolis.university>
2022-05-04 18:27:24 +0300

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
-import 'bloc/bloc_main.dart';
+import 'bloc/main_bloc.dart';
 
 void main() {
   runApp(const MyApp());
