24a999df06dd7e14c39bff8ae95cd26c5fd10911
gfx73 <khuzin.ja@yandex.ru>
2022-05-06 02:50:42 +0300

    Part of WebSocketProvider for waiting room written

--- lib/data/web_socket_provider.dart
+++ lib/data/web_socket_provider.dart
@@ -1,17 +1,26 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:arithmetic_pvp/bloc/events/waiting_room_events.dart';
+import 'package:arithmetic_pvp/bloc/events/rating_room_game_events.dart';
+import 'package:arithmetic_pvp/bloc/events/rating_room_statistics_events.dart';
 import 'package:arithmetic_pvp/bloc/events/web_socket_events.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:arithmetic_pvp/data/models/join_room_response.dart';
+import 'package:arithmetic_pvp/data/models/player.dart';
+
+import 'package:web_socket_channel/io.dart';
+
 
 class WebSocketProvider {
-  late final WebSocketChannel webSocketChannel;
+  late final IOWebSocketChannel webSocketChannel;
 
   final _url = "";
 
-  final _testUrl = "wss://192.168.31.116:8000/ws";
-
-  WebSocketProvider(){
-    webSocketChannel = WebSocketChannel.connect(
-      Uri.parse(_url),
+  WebSocketProvider(roomId){
+    final String _testUrl = "ws://192.168.31.124:8000/ws/rating_room/$roomId/";
+    webSocketChannel = IOWebSocketChannel.connect(
+      Uri.parse(_testUrl),
+      headers: {'Test-Name': 'user', 'Test-Email': 'user@gmail.com'}
     );
   }
 
@@ -19,8 +28,34 @@ class WebSocketProvider {
       .map((response) => _processServerResponse(response));
 
   _processServerResponse(response){
-    // TODO some processing
-    return WaitingRoomEventUsersLoading();
+    response = jsonDecode(response);
+    if (response.containsKey('action')){
+      switch (response['action']){
+        case 'player_joined': {
+          final List<Player> playersWaiting = response['players_waiting'].
+                        map((user) => Player.fromJson(user));
+          return WaitingRoomEventPlayerJoined(playersWaiting);
+        }
+        case 'all_players_joined': {
+          final room = JoinGameResponse.fromJson(response['room']);
+          return WaitingRoomEventAllPlayersJoined(room);
+        }
+        case 'task_solved': {
+          return RatingRoomGameEventUpdateProgressbar();
+        }
+      }
+    }
+    switch (response['reponse_to']){
+      case 'get_tasks': {
+        return RatingRoomGameEventTasksReceived();
+      }
+      case 'submit': {
+        return RatingRoomGameEventTaskReport();
+      }
+      case 'exit': {
+        return RatingRoomStatisticsEventReceived();
+      }
+    }
   }
 
   _submitAnswer(answer){
