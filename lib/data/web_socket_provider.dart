31be2d744b059e9cf252b277151614f00013bad5
gfx73 <khuzin.ja@yandex.ru>
2022-05-06 15:43:11 +0300

    Events and models added. WebSocketProvider provides events

--- lib/data/web_socket_provider.dart
+++ lib/data/web_socket_provider.dart
@@ -7,58 +7,84 @@ import 'package:arithmetic_pvp/bloc/events/rating_room_statistics_events.dart';
 import 'package:arithmetic_pvp/bloc/events/web_socket_events.dart';
 import 'package:arithmetic_pvp/data/models/join_room_response.dart';
 import 'package:arithmetic_pvp/data/models/player.dart';
+import 'package:arithmetic_pvp/data/models/player_progress.dart';
+import 'package:arithmetic_pvp/data/models/rating_room_stats.dart';
+import 'package:arithmetic_pvp/data/models/task_report.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import 'package:web_socket_channel/io.dart';
 
+import 'models/task.dart';
 
 class WebSocketProvider {
   late final IOWebSocketChannel webSocketChannel;
 
   final _url = "";
 
-  WebSocketProvider(roomId){
+  WebSocketProvider(roomId) {
     final String _testUrl = "ws://192.168.31.124:8000/ws/rating_room/$roomId/";
-    webSocketChannel = IOWebSocketChannel.connect(
-      Uri.parse(_testUrl),
-      headers: {'Test-Name': 'user', 'Test-Email': 'user@gmail.com'}
-    );
+    webSocketChannel = IOWebSocketChannel.connect(Uri.parse(_testUrl),
+        headers: {'Test-Name': 'user', 'Test-Email': 'user@gmail.com'});
   }
 
   Stream<WebSocketEvent> get webSocketStream => webSocketChannel.stream
       .map((response) => _processServerResponse(response));
 
-  _processServerResponse(response){
+  _processServerResponse(response) {
     response = jsonDecode(response);
-    if (response.containsKey('action')){
-      switch (response['action']){
-        case 'player_joined': {
-          final List<Player> playersWaiting = response['players_waiting'].
-                        map((user) => Player.fromJson(user));
-          return WaitingRoomEventPlayerJoined(playersWaiting);
-        }
-        case 'all_players_joined': {
-          final room = JoinGameResponse.fromJson(response['room']);
-          return WaitingRoomEventAllPlayersJoined(room);
-        }
-        case 'task_solved': {
-          return RatingRoomGameEventUpdateProgressbar();
-        }
+    if (response.containsKey('action')) {
+      switch (response['action']) {
+        case 'player_joined':
+          {
+            final List<Player> playersWaiting = response['players_waiting']
+                .map((user) => Player.fromJson(user))
+                .toList()
+                .cast<Player>();
+            return WaitingRoomEventPlayerJoined(playersWaiting);
+          }
+        case 'all_players_joined':
+          {
+            final room = JoinGameResponse.fromJson(response['room']);
+            return WaitingRoomEventAllPlayersJoined(room);
+          }
+        case 'task_solved':
+          {
+            final List<PlayerProgress> playerProgresses =response['progress']
+                .map((playerProgress) => PlayerProgress
+                  .fromJson(playerProgress))
+                .toList()
+                .cast<PlayerProgress>();
+            return RatingRoomGameEventUpdateProgressbar(playerProgresses);
+          }
       }
     }
-    switch (response['reponse_to']){
-      case 'get_tasks': {
-        return RatingRoomGameEventTasksReceived();
-      }
-      case 'submit': {
-        return RatingRoomGameEventTaskReport();
-      }
-      case 'exit': {
-        return RatingRoomStatisticsEventReceived();
-      }
+    switch (response['reponse_to']) {
+      case 'get_tasks':
+        {
+          final List<Task> tasks =
+              response['tasks'].map((task) => Task.fromJson(task))
+              .toList()
+              .cast<Task>();
+          return RatingRoomGameEventTasksReceived(tasks);
+        }
+      case 'submit':
+        {
+          response.remove('response_to');
+          final taskReport = TaskReport
+              .fromJson(response);
+          return RatingRoomGameEventTaskReport(taskReport);
+        }
+      case 'exit':
+        {
+          response.remove('response_to');
+          final stats = RatingRoomStats.fromJson(response);
+          return RatingRoomStatisticsEventReceived(stats);
+        }
     }
   }
 
-  _submitAnswer(answer){
+  _submitAnswer(answer) {
     webSocketChannel.sink.add(answer);
   }
 
