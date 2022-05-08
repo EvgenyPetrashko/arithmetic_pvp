98f5cf36f07ecf35a18849f93a707ee01a571ead
Shabalin13 <d.shabalin@innopolis.university>
2022-05-08 20:42:13 +0300

    deleted unnecessary thing

--- lib/bloc/main_bloc.dart
+++ lib/bloc/main_bloc.dart
@@ -15,14 +15,7 @@ import '../main.dart';
 class MainBloc extends Bloc<SplashScreenEvent, MainState> {
   MainBloc() : super(MainStateLoading()) {
     on<SplashScreenEventStartLoading>((event, emit) async {
-      await Firebase.initializeApp(
-        options: const FirebaseOptions(
-          apiKey: "AIzaSyD7MGUz4zwAixSE8M_vyd9wjRcbFs43jYs",
-          appId: "1:325644095359:web:15001e47b0caa290b4544a",
-          messagingSenderId: "325644095359",
-          projectId: "arithmetic-pvp-cf666",
-        ),
-      );
+      await Firebase.initializeApp();
 
       final _getIt = GetIt.instance;
 
